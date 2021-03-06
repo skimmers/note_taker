f9675b933771f4c7f5c3c4a494d349ac0600f178
Sharon Kim <skimilk19@gmail.com>
2021-03-06 16:06:08 -0500

    server is listening

--- server.js
+++ server.js
@@ -5,13 +5,30 @@ const fs = require("fs");
 
 // Telling node we'e creating "express" server
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 // Initialize notesData
 let notesData = [];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "Develope/public")));
+app.use(express.static(path.join(__dirname, "develop/public")));
 
 // Routes
+app.get("/api/notes", function(err, res){
+    try {
+        notesData = fs.readFileSync("develop/db/db.json", utf8);
+        notesData = JSON.parse(notesData);
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    res.json(notesData);
+});
+
+
+
+// Start the server on the port
+app.listen(PORT, function() {
+    console.log("SERVER IS LISTENING: " + PORT);
+  });
