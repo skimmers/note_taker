d88316e1ad302a97165779a827c7644e9be7fa18
Sharon Kim <skimilk19@gmail.com>
2021-03-06 15:32:10 -0500

    initialize notesData

--- server.js
+++ server.js
@@ -2,3 +2,16 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+// Telling node we'e creating "express" server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Initialize notesData
+let notesData = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "Develope/public")));
+
+// Routes
