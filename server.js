6601c5670e3e0b89c0ce63b6eafb78adbbc73f70
Sharon Kim <skimilk19@gmail.com>
2021-03-08 15:14:19 -0500

    new note json

--- server.js
+++ server.js
@@ -16,3 +16,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Develop/public")));
 
+// ROUTES
+app.get("/api/notes", function(err, res) {
+  try {
+    
+    notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+    notesData = JSON.parse(notesData);
+
+  } catch (err) {
+    console.log("\n error (in app.get.catch):");
+    console.log(err);
+  }
+  res.json(notesData);
+});
+
+// writes the new note to the json file
+app.post("/api/notes", function(req, res) {
+  try {
+    notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+    console.log(notesData);
+    notesData = JSON.parse(notesData);
+    req.body.id = notesData.length;
+    notesData.push(req.body);
+    notesData = JSON.stringify(notesData);
+    fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
+      if (err) throw err;
+    });
+    res.json(JSON.parse(notesData));
+
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
+});
